a87f6792ac71126fdd984eff0ef5e6cdf0fd7e10
Erik <erikdesjardins@users.noreply.github.com>
2016-07-29 17:00:25 -0400

    limit the Slack channel topic to 250 chars

--- src/main.rs
+++ src/main.rs
@@ -343,7 +343,7 @@ fn main() {
                         if let Some(by) = by {
                             post_message(&cli, &c.slack_token, &chan, &format!("*{}* has changed the topic", by), None);
                         }
-                        cli.set_topic(&chan, &topic.unwrap_or("".to_owned())).unwrap();
+                        cli.set_topic(&chan, &topic.unwrap_or("".to_owned()).chars().take(250).collect::<String>()).unwrap();
                     },
                     IrcToSlack::Kick { by, chans, nicks, reason } => {
                         let by = &by.unwrap_or("server".to_owned());
